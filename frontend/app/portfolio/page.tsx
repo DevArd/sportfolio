dafb1003c434a06863bc011c4afaf2bcf129c615
DevArd <arnaudplanas400@gmail.com>
2023-07-19 01:41:00 +0200

    get balance

--- frontend/app/portfolio/page.tsx
+++ frontend/app/portfolio/page.tsx
@@ -1,6 +1,22 @@
+"use client"
+import { MessiTokenAddress, MbappeTokenAddress } from '@/Constants'
 import React from 'react'
+import { useBalance, useAccount } from 'wagmi'
 
 export default function Page() {
+    const { address } = useAccount()
+    const balanceMessi = useBalance({
+        address: address,
+        token: MessiTokenAddress,
+    })
+    const balanceMBappe = useBalance({
+        address: address,
+        token: MbappeTokenAddress,
+    })
+
+    console.log('balanceMessi', balanceMessi)
+    console.log('balanceMBappe', balanceMBappe)
+
     return (
         <div>portfolio</div>
     )
